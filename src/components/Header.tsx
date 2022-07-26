4f71127cab5ea52ba24ea3217f344f3ad79139b4
Teodora Gjini <teodoragjini@yahoo.com>
2022-07-26 02:16:00 +0200

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import HeaderLeftMenu from "./HeaderLeftMenu"
+import SearchHeader from "./SearchHeader"
+
+
+function Header(){
+    return(
+        <header className="header">
+
+        <HeaderLeftMenu />
+ 
+        <SearchHeader />
+ 
+       </header>
+    )
+}
+
+export default Header
