7126ac8d4cc22d5573b79357c8ed512c175153a7
Teodora Gjini <teodoragjini@yahoo.com>
2022-07-26 03:26:37 +0200

    changes

---
+++ src/components/SearchHeader.tsx
@@ -0,0 +1,10 @@
+function SearchHeader(){
+    return (
+    <div className="search">
+    <input className="search-bar" placeholder="Search mail" />
+    </div>
+    )
+}
+
+
+export default SearchHeader
